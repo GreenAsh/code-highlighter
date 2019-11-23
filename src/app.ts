62d29e9660979d0c22047d5ae3cb2fe5f4888b7c
igor <moskalenkoip@gmail.com>
2019-11-24 03:51:30 +0500

    Import java as lang for Prism

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import * as Prism from "prismjs";
+
+console.log(Prism.highlight('var a = 1;', Prism.languages['java'], 'java'))
--- src/code-highlighter/config.ts
+++ src/code-highlighter/config.ts
@@ -1,3 +1,5 @@
+import 'prismjs/components/prism-java';
+
 export const LANGUAGES = ['clike', 'java'];
 export const DEFAULT_LANGUAGE = 'java';
 export const DOM = document;
