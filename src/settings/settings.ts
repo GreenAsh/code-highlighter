d782ce12a86124144761ba07873f2266cfcece31
igor <igor@miro.com>
2019-11-25 00:45:44 +0500

    Widget toolbar support

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -4,18 +4,35 @@ import {DefaultThemeRegistry} from "../code-highlighter/prism/themes/DefaultThem
 import {themeRegistry} from "../code-highlighter/prism/themes";
 
 export default {
-    _langKey: 'code.highlighter.lang',
+    _langKey_fallback_1: 'code.highlighter.lang',
+    _langKey: 'ch_lang',
     _themeKey: 'ch_theme',
     _defaultLang: DEFAULT_LANGUAGE,
     languages: LANGUAGES,
     getLang(): string {
-        return this._defaultLang;
-        // const lang = localStorage.getItem(this._langKey);
-        // if (this.languages[lang] === 1){
-        //     return lang;
-        // } else {
-        //     return this._defaultLang;
-        // }
+        const lang = this._getLang(this._langKey, this._getLang(this._langKey_fallback_1, this._defaultLang));
+        if (lang !== this._defaultLang){
+            this.setLang(lang); // overwrite fallback
+        }
+        return lang;
+    },
+    _getLang(propertyKey:string, defaultValue:string) : string {
+        const value = localStorage.getItem(propertyKey);
+        for (let i = 0; i < LANGUAGES.length; i++){
+            if (value === LANGUAGES[i]){
+                return value;
+            }
+        }
+        return defaultValue;
+    },
+    setLang(value:string) {
+        // todo cycle and previous one
+        for (let i = 0; i < LANGUAGES.length; i++){
+            if (value === LANGUAGES[i]){
+                localStorage.setItem(this._langKey, value);
+                localStorage.removeItem(this._langKey_fallback_1); // overwrite fallback key
+            }
+        }
     },
     getTheme(): string {
         let theme = localStorage.getItem(this._themeKey);
