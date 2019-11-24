3a4cb1af1dcf7ed15093a45fb89d892a8afe0348
GreenAsh <moskalenkoip@gmail.com>
2019-11-24 23:03:59 +0500

    Added theme support

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,7 +1,11 @@
 import {DEFAULT_LANGUAGE, LANGUAGES} from "../code-highlighter/config";
+import {ThemeContext} from "../code-highlighter/prism/themes/interfaces";
+import {DefaultThemeRegistry} from "../code-highlighter/prism/themes/DefaultThemeRegistry";
+import {themeRegistry} from "../code-highlighter/prism/themes";
 
 export default {
     _langKey: 'code.highlighter.lang',
+    _themeKey: 'ch_theme',
     _defaultLang: DEFAULT_LANGUAGE,
     languages: LANGUAGES,
     getLang(): string {
@@ -12,5 +16,15 @@ export default {
         // } else {
         //     return this._defaultLang;
         // }
+    },
+    getTheme(): string {
+        let theme = localStorage.getItem(this._themeKey);
+        if (theme == null) {
+            return themeRegistry.getTheme('default').getName();
+        }
+        return themeRegistry.getTheme(theme).getName();
+    },
+    setTheme(theme:string) {
+        localStorage.setItem(this._themeKey, themeRegistry.getTheme(theme).getName());
     }
 };
