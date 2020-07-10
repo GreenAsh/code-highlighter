684736b50c54fb672b9b35f36a6a423fd6339683
GreenAsh <moskalenkoip@gmail.com>
2020-07-10 11:42:37 +0300

    Update highlight-java-test.js

--- test/highlight-java-test.js
+++ test/highlight-java-test.js
@@ -7,28 +7,36 @@ const { JSDOM } = jsdom;
 const dom = new JSDOM(``);
 const document = dom.window.document;
 
-console.log(highlightCode(`package projects.pt.server.platform.application.models.platformpartner;
+console.log(highlightCode(`package com.example.spring.demo.oauth.infrastructure.configurations;
 
-import platform.server.core.general.spaces.model.ServerOnlyNotTypedModel;
-import platform.server.core.general.spaces.model.annotation.PersistentData;
-import platform.server.core.general.transaction.TransactionEnterPoint;
-import platform.server.core.space.annotations.models.ModelInfo;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
-@ModelInfo(value = "PlatformPartner", serverOnly = true)
-public class PlatformPartnerModel extends ServerOnlyNotTypedModel implements IPlatformPartner {
-
-    @PersistentData
-    private PlatformPartnerEntity entity;
+@AllArgsConstructor
+@EnableResourceServer
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
+@Configuration
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    private final TokenStore tokenStore;
 
     @Override
-    public boolean isPartner() {
-        return entity.isPartner();
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/api/**")
+                .authorizeRequests().antMatchers("/api/**")
+                .authenticated();
     }
 
-    @TransactionEnterPoint
     @Override
-    public void setPartner(boolean partner) {
-        change(entity).setPartner(partner);
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
     }
 }`, document, 'java'));
 
