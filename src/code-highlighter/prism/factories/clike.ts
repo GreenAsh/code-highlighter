460305a5d24358fced020b0012c70f0a18a895c0
GreenAsh <moskalenkoip@gmail.com>
2021-11-09 05:24:28 +0500

    Made keyword highlighting bold

--- src/code-highlighter/prism/factories/clike.ts
+++ src/code-highlighter/prism/factories/clike.ts
@@ -3,12 +3,13 @@ import {DOM} from "../../config";
 export class CLikeTokenFactory implements TokenFactory {
     create(name: String, className: string): Node | false | null {
         switch (name) {
+            case 'keyword':
+                return DOM.createElement('b', className);
             case 'string':
             case 'boolean':
             case 'number':
             case 'function':
             case 'comment':
-            case 'keyword':
             case 'class-name':
                 return DOM.createElement('span', className);
             case 'operator':
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
-version = '0.5.8'
+version = '0.5.9'
 
 module.exports = {
     mode: 'production',
