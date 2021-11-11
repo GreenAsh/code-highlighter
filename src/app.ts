b2db23fe8bb6bf207ad07159cd059c3109973ff0
igor <moskalenkoip@gmail.com>
2021-11-12 03:50:03 +0500

    Replaced line breaks transoformation according to @mlanin changes in forked repository

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,20 @@
 import {default as highlighter} from 'code-highlighter/prism'
+import {ThemeContext} from "./code-highlighter/prism/themes/interfaces";
+import {themeRegistry} from "./code-highlighter/prism/themes";
 
-highlighter.highlight('java', `public class MineSweeper
-{\tprivate int[][] myTruth;
-\tprivate boolean[][] myShow;
-\t
-\tpublic void cellPicked(int row, int col)
-\t{\tif( inBounds(row, col) && !myShow[row][col] )
-\t\t{\tmyShow[row][col] = true;
-\t\t
-\t\t\tif( myTruth[row][col] == 0)
-\t\t\t{\tfor(int r = -1; r <= 1; r++)
-\t\t\t\t\tfor(int c = -1; c <= 1; c++)
-\t\t\t\t\t\tcellPicked(row + r, col + c);
-\t\t\t}\t
-\t\t}
-\t}
-\t
-\tpublic boolean inBounds(int row, int col)
-\t{\treturn 0 <= row && row < myTruth.length && 0 <= col && col < myTruth[0].length;
-\t}
-}`).then(value => console.log(value)).catch(reason => console.error(reason));
+ThemeContext.getInstance().currentTheme = themeRegistry.getTheme('default');
+highlighter.highlight('java', `    /**
+     * Checks if a specific {@link StackTraceElement} in the current thread's stacktrace
+     * should cause devtools to be disabled.
+     * @param thread the current thread
+     * @return {@code true} if devtools should be enabled
+     */
+    public static boolean shouldEnable(Thread thread) {
+        for (StackTraceElement element : thread.getStackTrace()) {
+            if (isSkippedStackElement(element)) {
+                return false;
+            }
+        }
+        return true;
+    }
+`).then(value => console.log(value)).catch(reason => console.error(reason));
