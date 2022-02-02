7d4625f4e9089290ce30e08a25339e8df87f6db1
igor <moskalenkoip@gmail.com>
2022-02-02 21:55:46 +0500

    Add possibility to configure list of items shown in the widget menu fixes GH-11

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -5,6 +5,10 @@ import {settings} from "./shared-settings";
  * some doc
  */
 function initPage() {
+    settings.getMenuButtons().forEach((value, index) => {
+        initLanguages(value, index);
+    })
+
     const themeChooser = document.getElementById('themes_select');
     const themes = themeRegistry.listThemes()
     for (let i = 0; i < themes.length; i++) {
@@ -22,6 +26,25 @@ function initPage() {
             settings.setTheme(selectedTheme);
             miro.showNotification('saved');
         }
+    };
+}
+
+function initLanguages(value: string, index:number) {
+    const langSelector = document.getElementById('lang' + index) as HTMLSelectElement
+    const langArray = settings.getAvailableLanguages();
+    for (const lang of langArray) {
+        if (index === 0 && lang.key === 'none') {
+            continue
+        }
+        const langOption = document.createElement('option');
+        langOption.textContent = lang.displayName;
+        langOption.value = lang.key;
+        langOption.selected = lang.key === value;
+        langSelector.appendChild(langOption);
+    }
+    langSelector.onchange = () => {
+        const selectedLang = langSelector.options[langSelector.selectedIndex].value;
+        settings.setMenuButton(index, selectedLang);
     };
 }
 
