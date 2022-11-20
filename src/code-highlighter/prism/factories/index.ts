b9afcb47711569792147865f7063d5f9c19efbc4
GreenAsh <moskalenkoip@gmail.com>
2022-11-20 19:09:06 +0400

    Release/1.0.18 (#26)
    * Added support to C#, JSON (#25)
    * 1.0.18

--- src/code-highlighter/prism/factories/index.ts
+++ src/code-highlighter/prism/factories/index.ts
@@ -1,11 +1,15 @@
 import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-json';
 
+import { csharp } from './csharp'
+import { gherkin } from './gherkin'
 import { java } from './java'
 import { javascript } from './javascript'
-import { gherkin } from './gherkin'
 
 export const factories = new Map<String, TokenFactory>();
+factories.set('csharp', csharp);
+factories.set('gherkin', gherkin);
 factories.set('java', java);
 factories.set('js', javascript);
+factories.set('json', javascript);
 factories.set('ts', javascript);
-factories.set('gherkin', gherkin);
--- src/miro-plugin/icons.ts
+++ src/miro-plugin/icons.ts
@@ -1,15 +1,18 @@
 import mainData from '../../resources/icons/main.svg'
+import csharp from '../../resources/icons/csharp.svg'
 import javaData from '../../resources/icons/java.svg'
 import jsData from '../../resources/icons/js.svg'
+import jsonData from '../../resources/icons/json.svg'
 import tsData from '../../resources/icons/ts.svg'
 import cucumberData from '../../resources/icons/cucumber.svg'
-import {LanguageDescription} from "../settings/shared-settings";
 
 export const mainIcon = mainData;
 export const langKey2IconData: Map<string, string> = new Map([
+    ['csharp', csharp],
+    ['gherkin', cucumberData],
     ['java', javaData],
     ['js', jsData],
-    ['ts', tsData],
-    ['gherkin', cucumberData]
+    ['json', jsonData],
+    ['ts', tsData]
 ]);
 
